cb8aaacf1516c5bf0d82a429dafe88a7f57c5591
Yokesh-S03 <yogi1462001@gmail.com>
2025-05-07 15:57:57 +0530

    Fix: Added Render domain to ALLOWED_HOSTS

--- excel_processor/settings.py
+++ excel_processor/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-62tno&%c@ee-+csenc8gt753w!d)pjt3@vxc8dzogu(x_dzw_8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'excel-processor-coii.onrender.com']
 
 
 # Application definition
